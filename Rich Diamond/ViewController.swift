d4374d0a71b4376d160eda4307af4f0d4de25523
NithishaSathishkumar <nithisha132004@gmail.com>
2024-04-28 19:55:21 -0700

    Initial Commit

---
+++ Rich Diamond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rich Diamond
+//
+//  Created by Nithisha Sathishkumar on 4/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
